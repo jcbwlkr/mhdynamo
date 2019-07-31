3879b1af6bebd3940b599dbb2305cb548758fc2e
Jacob Walker <jacobwalker0814@gmail.com>
2019-07-31 11:43:20 -0500

    Initial implementation of all but Search

---
+++ dynamodb.go
@@ -0,0 +1,419 @@
+package mhdynamo
+
+// TODO(jlw) Make all of this safe for concurrent use.
+// TODO(jlw) Sprinkle some context on this.
+// TODO(jlw) get rid of so much nesting
+
+import (
+	"errors"
+	"fmt"
+	"log" // TODO(jlw) do not use the global logger.
+	"math"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/mailhog/data"
+)
+
+// message is the structure we store in DynamoDB.
+type message struct {
+	DayKey  string
+	ID      string
+	Expires int64
+	Msg     *data.Message
+}
+
+// Storage represents a DynamoDB powered storage backend for MailHog.
+type Storage struct {
+	client     *dynamodb.DynamoDB
+	table      string
+	consistent bool
+	ttl        int
+	now        func() time.Time // Use time.Now in production and a mock for tests.
+}
+
+// NewStorage creates a DynamoDB powered storage backend. Set consistent to
+// true to enforce strongly consistent reads. By default DynamoDB is intended
+// to be used in Eventual Consistency mode. Set ttl to a number of days that
+// messages should be kept.
+func NewStorage(client *dynamodb.DynamoDB, table string, consistent bool, ttl int) *Storage {
+	// TODO(jlw) ensure ttl is positive
+
+	return &Storage{
+		client:     client,
+		table:      table,
+		consistent: consistent,
+		ttl:        ttl,
+		now:        time.Now,
+	}
+}
+
+// idForMsg knows how to generate a string that encodes the dynamo ID for
+// a message. They ID has two parts: the ID of the message and the created
+// date. It is stored with the created date first as a unix timestamp in
+// nanoseconds because that representation is a fixed width and it's sortable.
+func idForMsg(m *data.Message) string {
+	var sb strings.Builder
+	sb.Grow(20 + len(m.ID)) // preallocate enough space. 19 for the timesamp, 1 for the |, plus the id
+	sb.WriteString(strconv.Itoa(int(m.Created.UnixNano())))
+	sb.WriteRune('|')
+	sb.WriteString(string(m.ID))
+	return sb.String()
+}
+
+// dayForID decodes the partition key (DayKey) from a storage ID. It will
+// be a string in YYYY-MM-DD format.
+func dayForID(id string) (string, error) {
+	if len(id) < 21 {
+		return "", errors.New("id is too short")
+	}
+
+	nano, err := strconv.Atoi(id[:19])
+	if err != nil {
+		return "", err
+	}
+	created := time.Unix(0, int64(nano))
+
+	return created.UTC().Format("2006-01-02"), nil
+}
+
+// daysForTTL gives the range of days that should be used as partition
+// keys for querying items inside the TTL range.
+func daysForTTL(ttl int, now time.Time) []string {
+	days := make([]string, ttl)
+
+	now = now.UTC()
+
+	for i := 0; i < ttl; i++ {
+		days[i] = now.Format("2006-01-02")
+		now = now.AddDate(0, 0, -1)
+	}
+
+	return days
+}
+
+// Store stores a message in DynamoDB and returns its storage ID.
+func (d *Storage) Store(m *data.Message) (string, error) {
+	msg := message{
+		DayKey:  m.Created.UTC().Format("2006-01-02"),
+		ID:      idForMsg(m),
+		Expires: m.Created.AddDate(0, 0, d.ttl).Unix(),
+		Msg:     m,
+	}
+
+	// Convert the mailhog message to a dynamodb map.
+	// TODO(jlw) This uses reflection and interfaces so it may be expensive. If
+	// profiling shows a problem we can do the mapping ourselves but it's
+	// complicated in creating each attribute with a type manually.
+	item, err := dynamodbattribute.MarshalMap(msg)
+	if err != nil {
+		return "", err // TODO(jlw) use pkg/errors?
+	}
+
+	input := &dynamodb.PutItemInput{
+		TableName: aws.String(d.table),
+		Item:      item,
+	}
+
+	if _, err = d.client.PutItem(input); err != nil {
+		return "", err // TODO(jlw) on error try again with exponential backoff until a time limit
+	}
+
+	return msg.ID, nil
+}
+
+// Count returns the number of stored messages.
+func (d *Storage) Count() int {
+
+	input := &dynamodb.ScanInput{
+		TableName:      aws.String(d.table),
+		Select:         aws.String("COUNT"),
+		ConsistentRead: aws.Bool(d.consistent),
+	}
+
+	scan, err := d.client.Scan(input)
+
+	// TODO(jlw) try again with exponential backoff
+
+	// TODO(jlw) for large tables Scan (especially frequent scans) can be very
+	// slow and use up all of your provisioned capacity. Consider alternatives
+	// here. For example we could have a second table that just maintains a
+	// count then ensure it is updated with just a sindle count record. Problems
+	// with that are synchronization and accounting for TTL.
+
+	// TODO(jlw) Existing implementations ignore the error on the Count step. This is
+	// generally a bad idea. If we can't count the db should we panic? Log it?
+	// This depends on how this method is used throughout the rest of MailHog.
+	if err != nil {
+		log.Printf("could not count table: %v", err)
+		return 0
+	}
+
+	return int(*scan.Count)
+}
+
+// Search finds messages matching the query
+func (d *Storage) Search(kind, query string, start, limit int) (*data.Messages, int, error) {
+	panic("not implemented")
+}
+
+// List returns a list of messages sorted by date created descending (newest
+// messages first). The list will include at most limit values and will begin
+// at the message indexed by start.
+func (d *Storage) List(start int, limit int) (*data.Messages, error) {
+
+	// TODO(jlw) document this
+
+	// TODO(jlw) do some ProjectionExpression queries for just ids to find the starting id? Maybe do a COUNT for each day until we pass the starting point.
+	s := make(data.Messages, 0, limit)
+
+	var skipped int
+	var pageErr error
+
+	// Call this in a loop once per day in the range
+	for _, day := range daysForTTL(d.ttl, d.now()) {
+
+		// Stop querying if we've hit our limit.
+		if len(s) >= limit {
+			break
+		}
+
+		input := &dynamodb.QueryInput{
+			TableName:              aws.String(d.table),
+			ConsistentRead:         aws.Bool(d.consistent),
+			Limit:                  aws.Int64(int64(limit - len(s))),
+			KeyConditionExpression: aws.String("DayKey = :day"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":day": &dynamodb.AttributeValue{S: aws.String(day)},
+			},
+			ScanIndexForward: aws.Bool(false),
+			// TODO(jlw) This doesn't account for the offset? Any way we can avoid the whole skip thing?
+		}
+
+		// Querying a table may require multiple calls. The sdk handles this for us
+		// by calling this function once per page.
+		fn := func(page *dynamodb.QueryOutput, hasNext bool) bool {
+			for _, item := range page.Items {
+				if skipped < start {
+					skipped++
+					continue
+				}
+
+				// Stop paginating if we've hit our limit.
+				if len(s) == limit {
+					return false
+				}
+
+				var m message
+				if err := dynamodbattribute.UnmarshalMap(item, &m); err != nil {
+					err = pageErr
+					return false
+				}
+				s = append(s, *m.Msg)
+			}
+
+			// If there are more pages we might not need to get back a full page worth
+			// of items. Lower the limit to just what we need. If our remaining limit
+			// is still more than a page no harm is done. If the remaining needs is
+			// less than a page then we'll only transmit what we need.
+			// TODO(jlw) I would like to do not ask for more than I need but modifying the Limit after pagination has started seems to be ignored.
+			if hasNext {
+				input.Limit = aws.Int64(int64(limit - len(s)))
+			}
+
+			return true
+		}
+
+		if err := d.client.QueryPages(input, fn); err != nil {
+			return nil, err // TODO(jlw) retry
+		}
+		if pageErr != nil {
+			return nil, pageErr // TODO(jlw) retry
+		}
+	}
+
+	return &s, nil
+}
+
+// DeleteOne deletes an individual message by storage ID
+func (d *Storage) DeleteOne(id string) error {
+	day, err := dayForID(id)
+	if err != nil {
+		return err
+	}
+
+	input := &dynamodb.DeleteItemInput{
+		TableName: aws.String(d.table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"DayKey": &dynamodb.AttributeValue{S: aws.String(day)},
+			"ID":     &dynamodb.AttributeValue{S: aws.String(id)},
+		},
+	}
+
+	if _, err := d.client.DeleteItem(input); err != nil {
+		return err // TODO(jlw) Implement backoff and retry
+	}
+
+	return nil
+}
+
+// DeleteAll deletes all messages stored in DynamoDB.
+func (d *Storage) DeleteAll() error {
+	// We know all of the partition keys that should have values because they
+	// have DayKeys in the range of our TTL. Loop over those days and get the IDs
+	// for that partition. Then batch delete those IDs.
+	//
+	// A more efficient option may be to delete the table and recreate it but we
+	// can't assume to have those permissions.
+
+	// TODO(jlw) do this concurrently
+
+	// Call this in a loop once per day in the range
+	for _, day := range daysForTTL(d.ttl, d.now()) {
+
+		input := &dynamodb.QueryInput{
+			TableName:              aws.String(d.table),
+			ConsistentRead:         aws.Bool(d.consistent),
+			KeyConditionExpression: aws.String("DayKey = :day"),
+			ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+				":day": &dynamodb.AttributeValue{S: aws.String(day)},
+			},
+			ProjectionExpression: aws.String("ID"), // This is the only field we want back.
+		}
+
+		var ids []string
+
+		// Querying a table may require multiple calls. The sdk handles this for us
+		// by calling this function once per page.
+		var pageErr error
+		fn := func(page *dynamodb.QueryOutput, hasNext bool) bool {
+			for _, item := range page.Items {
+				var m message
+				if err := dynamodbattribute.UnmarshalMap(item, &m); err != nil {
+					err = pageErr
+					return false
+				}
+				ids = append(ids, m.ID)
+			}
+			return true
+		}
+
+		if err := d.client.QueryPages(input, fn); err != nil {
+			return err // TODO(jlw) retry
+		}
+		if pageErr != nil {
+			return pageErr // TODO(jlw) retry
+		}
+
+		// Now that we have the IDs for a particular day we can batch delete them.
+		// These can only be done in batches of 25 at a time.
+		for len(ids) > 0 {
+			end := len(ids)
+			if end > 25 {
+				end = 25
+			}
+
+			batch := make([]*dynamodb.WriteRequest, end)
+			for i, id := range ids[:end] {
+				batch[i] = &dynamodb.WriteRequest{
+					DeleteRequest: &dynamodb.DeleteRequest{
+						// TODO(jlw) clean up all of this key stuff
+						Key: map[string]*dynamodb.AttributeValue{
+							"DayKey": &dynamodb.AttributeValue{S: aws.String(day)},
+							"ID":     &dynamodb.AttributeValue{S: aws.String(id)},
+						},
+					},
+				}
+			}
+			ids = ids[end:]
+
+			input := &dynamodb.BatchWriteItemInput{
+				RequestItems: map[string][]*dynamodb.WriteRequest{
+					d.table: batch,
+				},
+			}
+
+			// Start deleting. If any UnprocessedItems come back then try again.
+			for len(input.RequestItems) > 0 {
+				// TODO(jlw) exponential backoff
+
+				output, err := d.client.BatchWriteItem(input)
+				if err != nil {
+					return err
+				}
+				input.RequestItems = output.UnprocessedItems
+			}
+		}
+	}
+
+	return nil
+}
+
+// Load loads an individual message by storage ID
+func (d *Storage) Load(id string) (*data.Message, error) {
+	day, err := dayForID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String(d.table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"DayKey": &dynamodb.AttributeValue{S: aws.String(day)},
+			"ID":     &dynamodb.AttributeValue{S: aws.String(id)},
+		},
+		ConsistentRead: aws.Bool(d.consistent),
+	}
+
+	output, err := d.client.GetItem(input)
+	if err != nil {
+		return nil, err // TODO(jlw) Implement backoff and retry
+	}
+
+	if output.Item == nil {
+		return nil, fmt.Errorf("dynamodb: message %q not found", id) // TODO(jlw) pkg/errors?
+	}
+
+	var m message
+	if err := dynamodbattribute.UnmarshalMap(output.Item, &m); err != nil {
+		return nil, err
+	}
+
+	return m.Msg, nil
+}
+
+// These set the lower and upper bounds for the delay between attempts to retry
+// an API operation.
+const (
+	minBackoffInterval = 10 * time.Millisecond
+	maxBackoffInterval = 20 * time.Second
+)
+
+// retryInterval gives an exponentially increasing interval between attempts.
+// It includes some random variance and is bounded to only return values
+// between minBackoffInterval and maxBackoffInterval.
+func retryInterval(attempt int) time.Duration {
+	// 2^n results in 2, 4, 8, 16, 32 ms etc.
+	n := int(math.Pow(2, float64(attempt)))
+
+	// Add 1 to 20 ms for variance. Reduces chance of multiple instances calling
+	// in synchronized waves.
+	// TODO(jlw) does it matter that rand isn't seeded?
+	n += rand.Intn(20) + 1
+
+	// Convert to milliseconds.
+	delay := time.Duration(n) * time.Millisecond
+
+	// Enforce lower and upper bounds.
+	if delay < minBackoffInterval {
+		delay = minBackoffInterval
+	} else if delay > maxBackoffInterval {
+		delay = maxBackoffInterval
+	}
+	return delay
+}
---
+++ dynamodb_test.go
@@ -0,0 +1,307 @@
+package mhdynamo
+
+import (
+	"context"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/go-cmp/cmp"
+	"github.com/mailhog/data"
+)
+
+// TestIDFromMsg exercises the function for encoding messages into storage ids.
+func TestIDFromMsg(t *testing.T) {
+	msg := data.Message{
+		ID:      "id",
+		Created: time.Unix(1234567890, 1),
+	}
+	want := "1234567890000000001|id"
+	got := idForMsg(&msg)
+	if got != want {
+		t.Errorf("idForMsg(%+v) = %q, want %q", msg, got, want)
+	}
+}
+
+// TestDayForID calculates the Partition Key (Day) for an ID.
+func TestDayForID(t *testing.T) {
+	id := "1234567890000000001|id"
+	want := "2009-02-13"
+
+	got, err := dayForID(id)
+	if err != nil {
+		t.Fatalf("dayForID(%q) should not err, got %v", id, err)
+	}
+	if got != want {
+		t.Errorf("dayForID(%q) = day %q, want %q", id, got, want)
+	}
+}
+
+func TestDaysForTTL(t *testing.T) {
+	// With 7 days ttl, a query ran on 2019-09-04 should include messages from
+	// that date and the 6 days prior.
+	ttl := 7
+	now := time.Date(2019, time.September, 04, 12, 00, 00, 00, time.UTC)
+	want := []string{
+		"2019-09-04",
+		"2019-09-03",
+		"2019-09-02",
+		"2019-09-01",
+		"2019-08-31",
+		"2019-08-30",
+		"2019-08-29",
+	}
+
+	got := daysForTTL(ttl, now)
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("daysForTTL(%d, %v) did not match:\n%s", ttl, now, diff)
+	}
+}
+
+func TestStorage(t *testing.T) {
+	t.Run("CRUD", CRUD)
+	t.Run("DeleteAll", DeleteAll)
+	t.Run("List", List)
+}
+
+// CRUD runs a series of tests to exercise Count, Store, Load, and DeleteOne operations.
+func CRUD(t *testing.T) {
+	t.Parallel()
+
+	d, teardown := setup(t, "CRUD")
+	defer teardown()
+
+	if count := d.Count(); count != 0 {
+		t.Errorf("At start the table has %d items, expected 0", count)
+	}
+
+	msg := &data.Message{
+		ID:      mustMessageID(t),
+		Created: time.Now(),
+		From:    data.PathFromString("jacob@example.com"),
+		To:      []*data.Path{data.PathFromString("anna@example.com")},
+	}
+
+	id, err := d.Store(msg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if count := d.Count(); count != 1 {
+		t.Errorf("After storing one item the table has %d items, expected 1", count)
+	}
+
+	stored, err := d.Load(id)
+	if err != nil {
+		t.Errorf("Unable to load item by id %q: %v", id, err)
+	}
+
+	if diff := cmp.Diff(msg, stored); diff != "" {
+		t.Errorf("Stored message different from expected:\n%s", diff)
+	}
+
+	if err := d.DeleteOne(id); err != nil {
+		t.Errorf("Unable to delete item by id %q: %v", id, err)
+	}
+
+	if stored, err := d.Load(id); err == nil {
+		t.Errorf("Should get error when loading deleted item %q", id)
+		t.Errorf("Was able to load this: %+v", stored)
+	}
+}
+
+// DeleteAll is focused on testing the DeleteAll method.
+func DeleteAll(t *testing.T) {
+	t.Parallel()
+
+	d, teardown := setup(t, "DeleteAll")
+	defer teardown()
+
+	for i := 0; i < 3; i++ {
+		msg := &data.Message{
+			ID:      mustMessageID(t),
+			Created: time.Now(),
+			From:    data.PathFromString("jacob@example.com"),
+			To:      []*data.Path{data.PathFromString("anna@example.com")},
+		}
+		if _, err := d.Store(msg); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	if count := d.Count(); count != 3 {
+		t.Errorf("After storing items the table has %d items, expected 3", count)
+	}
+
+	if err := d.DeleteAll(); err != nil {
+		t.Errorf("Unable to delete all: %v", err)
+	}
+
+	if count := d.Count(); count != 0 {
+		t.Errorf("After calling DeleteAll the table has %d items, expected 0", count)
+	}
+}
+
+// List is focused on testing the List method.
+func List(t *testing.T) {
+	t.Parallel()
+
+	d, teardown := setup(t, "List")
+	defer teardown()
+
+	// Insert 40 messages with specially crafted creation dates so we can assert
+	// the order and offset of the messages returned from List. They will be over
+	// a range of dates to exercise different partition keys.
+	original := makeListMessages(t, 0, 40)
+	for i := range original {
+		if _, err := d.Store(&original[i]); err != nil {
+			t.Fatal(err)
+		}
+	}
+	return
+
+	// Let's say we're running these queries at noon on 2008-08-14.
+	d.now = func() time.Time { return time.Date(2008, time.August, 14, 12, 0, 0, 0, time.UTC) }
+
+	// First check we can get them all back in the right order.
+	got, err := d.List(0, 40)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if diff := cmp.Diff(&original, got); diff != "" {
+		t.Errorf("got %d items", len(*got))
+		t.Errorf("Listing all did not match expected:\n%s", diff)
+	}
+
+	// Next get just messages 10 through 19 (start 10, limit 10).
+	got, err = d.List(10, 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := makeListMessages(t, 10, 10)
+	if diff := cmp.Diff(&want, got); diff != "" {
+		t.Errorf("Got %d items, want %d", len(*got), len(want))
+		t.Errorf("Listing only 10 did not match expected:\n%s", diff)
+	}
+}
+
+func makeListMessages(t *testing.T, start, size int) data.Messages {
+	now := time.Date(2008, time.August, 14, 12, 0, 0, 0, time.UTC)
+
+	// Skip date up to the starting point.
+	for i := 0; i < start; i++ {
+		now = now.Add(-1 * time.Hour)
+	}
+
+	msgs := make(data.Messages, size)
+
+	// Make up to size messages all 1 hour apart. This should give us a spread
+	// that touches multiple partitions.
+	for i := 0; i < size; i++ {
+		id := strconv.Itoa(start + i)
+		msgs[i] = data.Message{
+			ID:      data.MessageID(id),
+			Created: now,
+			From:    data.PathFromString("jacob@example.com"),
+			To:      []*data.Path{data.PathFromString("anna@example.com")},
+		}
+		now = now.Add(-1 * time.Hour)
+	}
+	return msgs
+}
+
+func setup(t *testing.T, table string) (d *Storage, teardown func()) {
+	t.Helper()
+
+	table = "test" + table
+
+	sess, err := session.NewSession()
+	if err != nil {
+		t.Fatal("Could not create aws session: ", err)
+	}
+	db := dynamodb.New(sess)
+	db.Config = *db.Config.WithLogLevel(aws.LogDebugWithHTTPBody)
+	//db.AddDebugHandlers() // Turn this on to see a ton of AWS output.
+
+	// Start creating a table for these tests. It should have Day set as the
+	// Partition Key ("HASH") and ID as the Sort Key ("RANGE")
+	_, err = db.CreateTable(&dynamodb.CreateTableInput{
+		TableName: aws.String(table),
+
+		// Define which attributes make up the primary key.
+		KeySchema: []*dynamodb.KeySchemaElement{
+			&dynamodb.KeySchemaElement{
+				AttributeName: aws.String("DayKey"),
+				KeyType:       aws.String("HASH"),
+			},
+			&dynamodb.KeySchemaElement{
+				AttributeName: aws.String("ID"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+
+		// Define data type for attributes: Day and ID are strings.
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			&dynamodb.AttributeDefinition{
+				AttributeName: aws.String("DayKey"),
+				AttributeType: aws.String("S"),
+			},
+			&dynamodb.AttributeDefinition{
+				AttributeName: aws.String("ID"),
+				AttributeType: aws.String("S"),
+			},
+		},
+
+		// Set billing parameters to try to stay free-tier eligible.
+		BillingMode: aws.String("PROVISIONED"),
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(3),
+			WriteCapacityUnits: aws.Int64(3),
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Wait for that table to be in the ACTIVE state. Specify a wait interval of
+	// 500ms between checks otherwise the SDK will wait 20 seconds between each.
+	err = db.WaitUntilTableExistsWithContext(
+		context.Background(),
+		&dynamodb.DescribeTableInput{TableName: aws.String(table)},
+		request.WithWaiterDelay(retryInterval),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a DynamoDB storage with consistent reads enabled.
+	d = NewStorage(db, table, true, 7)
+
+	// Define teardown func to be called later for cleanup.
+	teardown = func() {
+		t.Helper()
+
+		// Delete the table we created for these tests.
+		_, err := db.DeleteTable(&dynamodb.DeleteTableInput{
+			TableName: aws.String(table),
+		})
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	return d, teardown
+}
+
+func mustMessageID(t *testing.T) data.MessageID {
+	t.Helper()
+	id, err := data.NewMessageID("localhost")
+	if err != nil {
+		t.Fatal(err)
+	}
+	return id
+}
